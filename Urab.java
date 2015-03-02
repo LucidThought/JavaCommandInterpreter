caf3713e771cccb8390398ae287429481813cc12
Riley Lahd, Esquire <rileylahd@gmail.com>
2015-03-02 14:48:26 -0700

    added verbose flag and help message var

--- Urab.java
+++ Urab.java
@@ -1,18 +1,34 @@
+/*
+CPSC 449 - Java Assignment - Java Command Interpreter
+Anthony Coulthard
+Andrew Howell
+Riley Lahd
+Andrew Lata
+Kendra Wannamaker
+
+Version 1.0.0 - 02/03/2015
+
+Our language is named Urab. This file will import a command set and
+take command line input according to that command set.
+*/
 import java.io.*;
 import java.util.Scanner;
 
 
 public class Urab
 {
+    String HELP = ""
     public static void main(String[] args)
     {   
+        boolean verbose = false
         Scanner in = new Scanner(System.in);
         String input = "";
         while(!input.equals("q"))
         {
-        System.out.print("> ");
-        input = in.nextLine();
-        System.out.print(input + "\n");
+            System.out.print("> ");
+            input = in.nextLine();
+            //System.out.print(input + "\n");
+            
         }
     }
 }
