66fc3241235f054f02959fc1f4ec344f1d8e5ea2
LucidThought <alata.it@gmail.com>
2015-03-06 15:56:23 -0700

    Constructor for new Spyglass completed. urab.

--- Spyglass.java
+++ Spyglass.java
@@ -1,10 +1,15 @@
 
+import java.net.URLClassLoader;
 import java.net.JarURLConnection;
 import java.io.FileNotFoundException;
+import java.util.jar.JarFile;
+import java.util.jar.JarEntry;
+import java.util.Enumeration;
 
 public class Spyglass
 {
 	private JarURLConnection jarConnection;
+	private Class lookAtThis;
 
 	public Spyglass(String jarFile, String classFile) throws FileNotFoundException
 	{
@@ -12,8 +17,26 @@ public class Spyglass
 		//jarConnection = (JarURLConnection)comJar.openConnection();
 		if (verifyClass(jarFile, classFile) == true)
 		{
-			URL comJar = new URL("jar", "", jarfile);
-			jarConnection = (JarURLConnection)comJar.openConnection();
+			JarFile jar = new JarFile(jarFile);
+			Enumeration e = jarFile.entries();
+
+			URL[] urls = { new URL("jar:file:" + jarFile+"!/") };
+			URLClassLoader cl = URLClassLoader.newInstance(urls);
+
+			while (e.hasMoreElements()) 
+			{
+				JarEntry je = (JarEntry) e.nextElement();
+				if(je.isDirectory() || !je.getName().endsWith(".class"))
+				{
+				continue;
+				}
+			// -6 because of .class
+			String className = je.getName().substring(0,je.getName().length()-6);
+			className = className.replace('/', '.');
+			if (className.substring(className.lastIndexOf(".")).equalsIgnoreCase(classFile))
+			lookAtThis = cl.loadClass(className);
+			break;
+			}
 		}
 		else
 		{
